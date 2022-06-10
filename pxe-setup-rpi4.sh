2ac28d28f80bce31335d5eb26febd55aa1f4965d
Jacob Magnusson <m@jacobian.se>
2022-06-10 12:39:29 +0200

    fix: Use more variables

--- pxe-setup-rpi4.sh
+++ pxe-setup-rpi4.sh
@@ -4,6 +4,8 @@ PI_SERIAL_NUMBER="$1"
 USERNAME="$2"
 SSH_PUBLIC_KEY="$3"
 STORED_IMAGE_PATH="$4"
+NFS_DIR=/srv/nfs/$PI_SERIAL_NUMBER
+TFTP_DIR=/srv/tftp/$PI_SERIAL_NUMBER
 
 [[ -z "$1" ]] && echo "Pass Raspberry PI serial number as argument #1" && exit 1
 [[ -z "$2" ]] && echo "Pass username as argument #2" && exit 1
@@ -45,43 +47,39 @@ mount /dev/mapper/loop*p2 rootmnt/
 
 mkdir -p /srv/{nfs,tftp}/${PI_SERIAL_NUMBER} || true
 # NOTE: We allow this failure of the following error:
-#       "cp: preserving permissions for ‘/srv/nfs/$PI_SERIAL_NUMBER/var/log/journal’: Operation not supported"
-cp -a rootmnt/* /srv/nfs/${PI_SERIAL_NUMBER} || true
-cp -a bootmnt/* /srv/nfs/${PI_SERIAL_NUMBER}/boot/
+#       "cp: preserving permissions for ‘$NFS_DIR/var/log/journal’: Operation not supported"
+cp -a rootmnt/* $NFS_DIR || true
+cp -a bootmnt/* $NFS_DIR/boot/
 umount bootmnt/
 umount rootmnt/
 cd "$CURRDIR"
 rm -r /tmp/pxestuff
 
-# rm /srv/nfs/${PI_SERIAL_NUMBER}/boot/start4.elf
-# rm /srv/nfs/${PI_SERIAL_NUMBER}/boot/fixup4.dat
-# wget https://github.com/raspberrypi/rpi-firmware/raw/master/start4.elf \
-#     -P /srv/nfs/${PI_SERIAL_NUMBER}/boot/
-# wget https://github.com/raspberrypi/rpi-firmware/raw/master/fixup4.dat \
-#     -P /srv/nfs/${PI_SERIAL_NUMBER}/boot/
-
-grep "/srv/nfs/${PI_SERIAL_NUMBER}/boot" /etc/fstab > /dev/null \
-    || echo "/srv/nfs/${PI_SERIAL_NUMBER}/boot /srv/tftp/${PI_SERIAL_NUMBER} none defaults,bind 0 0" \
+grep "$NFS_DIR/boot" /etc/fstab > /dev/null \
+    || echo "$NFS_DIR/boot $TFTP_DIR none defaults,bind 0 0" \
     >> /etc/fstab
 
-grep "/srv/nfs/${PI_SERIAL_NUMBER}" /etc/exports > /dev/null \
-    || echo "/srv/nfs/${PI_SERIAL_NUMBER} *(rw,sync,no_subtree_check,no_root_squash)" \
+grep "$NFS_DIR" /etc/exports > /dev/null \
+    || echo "$NFS_DIR *(rw,sync,no_subtree_check,no_root_squash)" \
     >> /etc/exports
 
-mount /srv/tftp/${PI_SERIAL_NUMBER}/
+mount $TFTP_DIR
 # Enable SSH from first boot
-touch /srv/nfs/${PI_SERIAL_NUMBER}/boot/ssh
-sed -i /UUID/d /srv/nfs/${PI_SERIAL_NUMBER}/etc/fstab
-echo "console=serial0,115200 console=tty root=/dev/nfs nfsroot=${PRIMARY_SERVER_IP}:/srv/nfs/${PI_SERIAL_NUMBER},vers=3 rw ip=dhcp rootwait elevator=deadline" \
-    > /srv/nfs/${PI_SERIAL_NUMBER}/boot/cmdline.txt
+touch $NFS_DIR/boot/ssh
+sed -i /UUID/d $NFS_DIR/etc/fstab
+echo "console=serial0,115200 console=tty root=/dev/nfs nfsroot=${PRIMARY_SERVER_IP}:$NFS_DIR,vers=3 rw ip=dhcp rootwait elevator=deadline" \
+    > $NFS_DIR/boot/cmdline.txt
 
 # Create a user with a random password (we'll be using SSH public key login instead)
 # https://www.raspberrypi.com/documentation/computers/configuration.html#configuring-a-user
 PASSWORD=$(echo $RANDOM | md5sum | head -c 30)
-{ echo "${USERNAME}:"; echo "${PASSWORD}" | openssl passwd -6 -stdin;} | tr -d '[:space:]' > /srv/tftp/${PI_SERIAL_NUMBER}/userconf.txt
+{ echo "${USERNAME}:"; echo "${PASSWORD}" | openssl passwd -6 -stdin;} | tr -d '[:space:]' > $TFTP_DIR/userconf.txt
+
+# Set hostname to PI's serial number
+echo $PI_SERIAL_NUMBER > $NFS_DIR/etc/hostname
 
 # Set up authorized SSH key
-USER_DIR=/srv/nfs/$PI_SERIAL_NUMBER/home/$USERNAME
+USER_DIR=$NFS_DIR/home/$USERNAME
 mkdir -p $USER_DIR/.ssh
 USER_AND_GROUP="$(stat -c %u:%g $USER_DIR)"
 echo "$SSH_PUBLIC_KEY" > $USER_DIR/.ssh/authorized_keys
